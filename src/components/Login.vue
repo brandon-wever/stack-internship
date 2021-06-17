38f1626e1a4310af996726b8b8579568be4fe97a
Brandon Wever <brandon.w3ver@gmail.com>
2021-06-17 08:59:04 -0400

    Adding a new component called login and adding that to our app.

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <h2>Login</h2>
+
+    <form>
+        <label for="fname">Email:</label><br>
+        <input type="email" id="email" name="email"><br>
+        <label for="lname">Password:</label><br>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Submit">
+    </form> 
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  name: 'Login',
+  components: {
+    
+  }
+})
+</script>
+
+<style>
+
+</style>
