9d02b97722a975b4c6b006b8e9760781efa412ff
Brandon Wever <brandon.w3ver@gmail.com>
2021-06-18 08:39:57 -0400

    Put in alert that reprints user's email & password

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,12 @@ export default defineComponent({
   name: 'App',
   components: {
     LoginVue
-  }
+  },
+  data() {
+    return {
+     
+    }
+  } 
 })
 </script>
 
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,12 +2,12 @@
     <h2>Login</h2>
 
     <form>
-        <label for="fname">Email:</label><br>
-        <input type="email" id="email" name="email"><br>
-        <label for="lname">Password:</label><br>
-        <input type="password" id="password" name="password"><br><br>
-        <input type="submit" value="Submit">
-    </form> 
+      <label for="email">Email:</label><br>
+      <input type="email" id="email" name="email" v-model="email"><br>
+      <label for="password">Password:</label><br>
+      <input type="password" id="password" name="password" v-model="password"><br><br>
+      <button @click="submit">Submit</button>
+    </form>
 </template>
 
 <script lang="ts">
@@ -17,7 +17,18 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'Login',
   components: {
-    
+
+  },
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    submit() {
+        alert(`Email: ${this.email}, Password: ${this.password}`);
+    } 
   }
 })
 </script>
