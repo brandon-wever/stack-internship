b1bf0f37b0f2c8c13e65b7bf4541801d86123daf
Brandon Wever <brandon.w3ver@gmail.com>
2021-06-23 08:37:04 -0400

    Added form validation to register.vue

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -7,11 +7,14 @@
     <div class="form-body">
       <label for="email">Email:</label>
       <input type="email" id="email" name="email" v-model="email">
-      <!-- TODO: Show nice red error message below input when field is invalid after submitting -->
+      <label class="error-message" v-if="isInputEmpty(email) && doesFormHaveError"> email is required </label>
       <label for="password">Password:</label>
       <input type="password" id="password" name="password" v-model="password">
+      <label class="error-message" v-if="isInputEmpty(password) && doesFormHaveError"> password is required </label>
       <label for="retypePassword">Retype Password:</label>
       <input type="password" id="retypePassword" name="retypePassword" v-model="retypePassword">
+      <label class="error-message" v-if="isInputEmpty(retypePassword) && doesFormHaveError"> retyped password is required </label>
+      <label class="error-message" v-if="password !== retypePassword && doesFormHaveError"> Passwords need to match </label>
       <button @click="submit">Submit</button>
       <router-link to="/">Go to back if you already have account</router-link>
     </div>
@@ -32,30 +35,40 @@ export default defineComponent({
     return {
       email: "",
       password: "",
-      retypePassword: ""
+      retypePassword: "",
+      doesFormHaveError: false
     }
   },
   methods: {
-    isInputNotEmpty(input: string, label: string) {
+    isInputEmpty(input: string, label: string = "") {
         if (input.length === 0) {
-            alert(`Please enter an ${label}`);
-            return false;
+            label ? alert(`Please enter an ${label}`) : null;
+            return true;
         }
 
-        return true;
+        return false;
     },
     submit() {
-        if (this.isInputNotEmpty(this.email, 'email')) {
+        if (this.isInputEmpty(this.email, 'email')) {
+            this.doesFormHaveError = true;
             return;
         }
-        if (this.isInputNotEmpty(this.password, 'password')) {
+        if (this.isInputEmpty(this.password, 'password')) {
+            this.doesFormHaveError = true;
             return;
         }
-        if (this.isInputNotEmpty(this.retypePassword, 'retype password')) {
+        if (this.isInputEmpty(this.retypePassword, 'retype password')) {
+            this.doesFormHaveError = true;
             return;
         }
 
-        // TODO: Verify password and retype password are the same
+        if(this.password !== this.retypePassword) {
+            alert('Please make sure password matched retype password');
+            this.doesFormHaveError = true;
+            return;
+        }
+
+        this.doesFormHaveError = false;
 
         alert(`Email: ${this.email}, Password: ${this.password}, Password: ${this.retypePassword}`);
     } 
@@ -77,5 +90,9 @@ export default defineComponent({
 
   .spacer {
     flex: 4;
+  }
+
+  .error-message{
+      color: red;
   }
 </style>
